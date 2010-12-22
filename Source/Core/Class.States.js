7ca0ab8e4858baf316a92f616498b809ace054fe
Pavel Pravosud <rwz@duckroll.ru>
2010-12-22 18:37:05 +0700

    Class.States jslint && 1.3-nocompat

--- Source/Core/Class.States.js
+++ Source/Core/Class.States.js
@@ -21,54 +21,72 @@ provides:
 ...
 */
 
+(function(){
+  
+  function stringType(value){
+    return 'string' === typeOf(value);
+  }
+  
+  function defined(value){
+    return value != null;
+  }
+  
+  
+  
+  Class.Stateful = function(states) {
+    var proto = {
+      options: {
+        states: {}
+      },
+      setStateTo: function(state, to) {
+        return this[this.options.states[state][to ? 'enabler' : 'disabler']]();
+      }
+    };
 
-Class.Stateful = function(states) {
-  var proto = {
-    options: {
-      states: {}
-    },
-    setStateTo: function(state, to) {
-      return this[this.options.states[state][to ? 'enabler' : 'disabler']]()
-    }
+    Object.each(states, function(methods, state) {
+      var options = Array.link(methods, {
+        enabler: stringType,
+        disabler: stringType,
+        toggler: stringType,
+        reflect: defined
+      });
+      
+      //enable reflection by default
+      if (!defined(options.reflect)) options.reflect = true;
+
+      proto.options.states[state] = options;
+
+      proto[options.enabler] = function() {
+        if (this[state]) return false;
+        this[state] = true; 
+
+      	if (Class.hasParent(this)) this.parent.apply(this, arguments);
+
+        this.fireEvent(options.enabler, arguments);
+        if (this.onStateChange && options.reflect) this.onStateChange(state, true, arguments);
+        return true;
+      };
+
+      proto[options.disabler] = function() {
+        if (!this[state]) return false;
+        this[state] = false;
+
+    	  if (Class.hasParent(this)) this.parent.apply(this, arguments);
+
+        this.fireEvent(options.disabler, arguments);
+        if (this.onStateChange && options.reflect) this.onStateChange(state, false, arguments);
+        return true;
+      };
+
+      if (options.toggler) proto[options.toggler] = function() {
+        return this[this[state] ? options.disabler : options.enabler].apply(this, arguments);
+      };
+    });
+
+    return new Class(proto);
   };
 
-  Hash.each(states, function(methods, state) {
-    var options = Array.link(methods, {enabler: String.type, disabler: String.type, toggler: String.type, reflect: $defined})
-    //enable reflection by default
-    if (!$defined(options.reflect)) options.reflect = true;
-    
-    proto.options.states[state] = options;
-
-    proto[options.enabler] = function() {
-      if (this[state]) return false;
-      this[state] = true; 
-
-    	if (Class.hasParent(this)) this.parent.apply(this, arguments);
-    	
-      this.fireEvent(options.enabler, arguments);
-      if (this.onStateChange && options.reflect) this.onStateChange(state, true, arguments);
-      return true;
-    }
-
-    proto[options.disabler] = function() {
-      if (!this[state]) return false;
-      this[state] = false;
-
-  	  if (Class.hasParent(this)) this.parent.apply(this, arguments);
-
-      this.fireEvent(options.disabler, arguments);
-      if (this.onStateChange && options.reflect) this.onStateChange(state, false, arguments);
-      return true;
-    }
-
-    if (options.toggler) proto[options.toggler] = function() {
-      return this[this[state] ? options.disabler : options.enabler].apply(this, arguments)
-    }
-  });
-  
-  return new Class(proto)
-}
-
-Class.Mutators.States = function(states) {
-  this.implement('Includes', [Class.Stateful(states)]);
-};
+  Class.Mutators.States = function(states) {
+    this.implement('Includes', [Class.Stateful(states)]);
+  };
+})();
