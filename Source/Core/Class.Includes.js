d40950d2267f3b7dd8e58b43d7f35c753621ba19
Pavel Pravosud <rwz@duckroll.ru>
2010-12-22 17:55:28 +0700

    Class.Includes fixes && jslint

--- Source/Core/Class.Includes.js
+++ Source/Core/Class.Includes.js
@@ -18,54 +18,51 @@ provides: [Class.Mutators.Includes, Class.include, Class.flatten]
 */
 
 (function() {
-  function defined (a) {
-    return a != undefined;
-  }
   
   var getInstance = function(klass){
-  	klass.$prototyping = true;
-  	var proto = new klass;
-  	delete klass.$prototyping;
-  	return proto;
+    klass.$prototyping = true;
+    var proto = new klass;
+    delete klass.$prototyping;
+    return proto;
   };
   
   Class.include = function(klass, klasses) {
     return new Class({
       Includes: Array.from(arguments).flatten()
     });
-  }
+  };
   
   Class.flatten = function(items) {
-    return Array.from(items).filter(defined).map(function(item, i) {
+    return Array.from(items).clean().map(function(item, i) {
       if (item.parent) {
-        return [Class.flatten(item.parent), item]
+        return [Class.flatten(item.parent), item];
       } else {
-        return item
+        return item;
       }
     }).flatten();
-  }
+  };
 
   Class.Mutators.Includes = function(items) {
     var instance = this.parent ? this.parent : items.shift();
-  	Class.flatten(items).each(function(parent){
+    Class.flatten(items).each(function(parent){
       var baked = new Class;
       if (instance) {
         baked.parent = instance;
         baked.prototype = getInstance(instance);
       }
-      var proto = Object.append({}, parent.prototype)
+      var proto = Object.append({}, parent.prototype);
       delete proto.$caller;
       delete proto.$constructor;
       delete proto.parent;
       delete proto.caller;
       for (var i in proto) {
         var fn = proto[i];
-        if (fn && fn.$owner && (fn.$owner != parent) && fn.$owner.parent) delete proto[i]
+        if (fn && fn.$owner && (fn.$owner != parent) && fn.$owner.parent) delete proto[i];
       }
       baked.implement(proto);
-      instance = baked
-  	}, this);
-  	this.parent = instance
-  	this.prototype = getInstance(instance);
-  }
+      instance = baked;
+    }, this);
+    this.parent = instance;
+    this.prototype = getInstance(instance);
+  };
 })();
