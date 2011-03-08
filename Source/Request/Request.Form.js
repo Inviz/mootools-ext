a0add4f255c1511ee1d670c304b17135603bff10
Yaroslaff Fedin <me@inviz.ru>
2011-03-08 21:52:02 +0800

    Fix Request.Form to redirect on gets (therefore no extra ? in url)

--- Source/Request/Request.Form.js
+++ Source/Request/Request.Form.js
@@ -24,14 +24,16 @@ provides:
   var convert = function(thing, prefix) {
     var html = [];
     
-    switch ($type(thing)) {
-      case "object": case "hash":
+    switch (typeOf(thing)) {
+      case "object":
         Hash.each(thing, function(value, key) {
           html.push(convert(value, prefix ? (prefix + '[' + key + ']') : key));
         });
         break;
       case "array":
         for (var key = 0; key < thing.length; key++) html.push(convert(thing[key], prefix + '[]'));
+        break;
+      case "boolean":
         break;
       default:
         return ["<input type='hidden' name='", prefix, "' value='", thing.toString().replace(/\"/g, '\\"'), "'/>"].join("");
@@ -49,7 +51,7 @@ provides:
       method: "get",
       async: false,
 			form: null,
-			data: {}
+			data: null
     },
   
     initialize: function(options) {
@@ -61,12 +63,11 @@ provides:
     getData: function(data) {
       return data;
     },
-  
-    getForm: function(options, attrs) {
+    
+    getOptions: function(options) {
       options = $merge(this.options, options)
       var data = this.getData(options.data);
-      if (options.method == "get") data = '?' + Hash.toQueryString(data);
-      if ($type(data) == "string") {
+      if (data.indexOf) {
         var fragments = options.url.split('#');
         var bits = fragments[0].split('?');
         var params = '?' + (bits[1] || '');
@@ -74,24 +75,30 @@ provides:
         if (fragments[1]) options.url += "#" + fragments[1];
         data = null
       }
-      
+
       if (options.method != "get" && options.method != "post") {
         data._method = options.method
         options.method = "post"
       }
-      
+      if (data) options.data = data;
+      else delete options.data;
+      return options;
+    },
+  
+    getForm: function(options, attrs) {
       return (this.options.form || new Element('form', attrs).inject(document.body)).set({
         'method': options.method,
         'action': options.url,
-        'html'  : convert(data || {})
+        'html'  : convert(options.data)
       });
     },
 
     send: function(options) {
+      options = this.getOptions(options);
       this.fireEvent('request', options);
-      var form = this.getForm(options);
-      var method = (options || {}).method || this.options.method;
-      form.submit();
+      if (options.method == 'get') {
+        location.href = options.url;
+      } else this.getForm(options).submit();
     }
   })
 })();
