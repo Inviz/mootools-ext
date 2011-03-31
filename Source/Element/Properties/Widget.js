e159191238dc3864f1c5d67d06a6e817c26c768a
Yaroslaff Fedin <me@inviz.ru>
2011-03-31 19:06:33 +0800

    Remove some old cruft

--- Source/Element/Properties/Widget.js
+++ Source/Element/Properties/Widget.js
@@ -24,27 +24,7 @@ Element.Properties.widget = {
   },
 	
 	set: function(options) {
-	  var retrieved = Element.retrieve(this, 'widget');
-		if (retrieved) {
-			return retrieved.setOptions(options)
-		} else {
-			var given = Element.retrieve(this, 'widget:options') || {};
-			for (var i in options) {
-				if (given[i] && i.match('^on[A-Z]')) {
-					given[i] = (function(a,b) {        // temp solution (that is 1.5 years in production :( )
-						return function() {              // wrap two functions in closure instead of overwrite
-							a.apply(this, arguments);      // TODO: some way of passing a raw array of callbacks
-							b.apply(this, arguments);
-						}
-					})(given[i], options[i])
-				} else {
-					var o = {};
-					o[i] = options[i];
-					$extend(given, o);
-				}
-			}
-			Element.retrieve(this, 'widget:options', given);
-		}
+	  
 	}
 };
 
