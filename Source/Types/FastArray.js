829861875242039b6539e4b2a9adc68f6d3bcaa7
Yaroslaff Fedin <invizko@gmail.com>
2011-05-19 15:20:45 +0800

    Introduce FastArray#toObject. Exports values as clean object

--- Source/Types/FastArray.js
+++ Source/Types/FastArray.js
@@ -64,5 +64,11 @@ FastArray.prototype = {
     var bits = [];
     for (var key in this) if (this.hasOwnProperty(key)) bits.push(key);
     return bits.join(delimeter)
+  },
+  
+  toObject: function() {
+    var object = {};
+    for (var key in this) if (this.hasOwnProperty(key)) object[key] = this[key];
+    return object;
   }
 };
