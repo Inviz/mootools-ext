faa7412ee5f19ca5488dc16ebaa52df854b243fe
Yaroslaff Fedin <invizko@gmail.com>
2011-09-09 18:51:10 +0800

    Handle serialization of arrays gracefully:
    Object.toQueryString({a: [1, {b: 2}]})
    "a[0]=1&a[1][b]=2"
    Object.toQueryString({a: [1, 2]})
    "a[]=1&a[]=2"

--- Source/Types/Object.js
+++ Source/Types/Object.js
@@ -13,22 +13,32 @@ extends: Core/Object
 */
 
 Object.extend({
-	toQueryString: function(object, base){
-		var queryString = [];
-		var serialize = function(value, key, multiple){
-			if (base) key = base + '[' + key + ']';
-			if (multiple == true) key += '[]';
-			var result;
-			switch (typeOf(value)){
-				case 'object': result = Object.toQueryString(value, key); break;
-				case 'array':
-					for (var i = 0, j = value.length; i < j; i++) serialize(value[i], key, true);
-				break;
-				default: result = key + '=' + encodeURIComponent(value);
-			}
-			if (value != null && result != null) queryString.push(result);
-		};
-		Object.each(object, serialize);
-		return queryString.join('&');
+	toQueryString: function(object, base, index){
+		if (typeof object == 'object') {
+  		var queryString = [];
+		  if (object.push) {
+				var complex = false;
+				for (var i = 0, j = object.length; i < j; i++) {
+				  var item = object[i];
+				  if (item != null) 
+				    if (typeof item == "object") {
+				      complex = true;
+				      break;
+				    }
+				}
+		    for (var i = 0; i < j; i++) {
+		      var key = base ? base + '[' + (complex ? i : '') + ']' : i;
+		      queryString.push(Object.toQueryString(object[i], key))
+		    }
+		  } else {
+		    for (var i in object) {
+		      var key = base ? base + '[' + i + ']' : i;
+		      queryString.push(Object.toQueryString(object[i], key))
+		    }
+		  }
+  		return queryString.join('&');
+		} else {
+		  return base + '=' + encodeURIComponent(object);
+		}
 	}
 });
